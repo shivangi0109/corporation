ee573754ecd598930b346b0a46136be73747dfe4
Shivangi Srivastava <shi06dec@gmail.com>
2024-12-09 17:06:35 -0700

    Linking between pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
+      Home Page
+    </div>
   );
 }

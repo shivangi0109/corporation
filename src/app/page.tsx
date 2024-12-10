42f8fa039757f3a86def489825a5f8e5124e0324
Shivangi Srivastava <shi06dec@gmail.com>
2024-12-10 13:26:24 -0700

    Add a Reusable Hero presentation component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import Image from 'next/image';
 import homeImg from '/public/home.jpg';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="car factory"
-          fill
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
-    </div>
+    <Hero imgData={homeImg} imgAlt="car factory" title="Professional Cloud Hosting" />
   );
 }
